0cb84be3249c95cc9bc83bebe35165d5190ca049
straho99 <strahil.ruychev@gmail.com>
2015-07-25 06:32:43 +0300

    Added more Sales Reports
    New methods for Sales reports by product for a period.

---
+++ DatabaseManager-1/DatabaseManager/DatabaseManager.SalesReports/ProductSales.cs
@@ -0,0 +1,15 @@
+﻿namespace DatabaseManager.SalesReports
+{
+    public class ProductSales
+    {
+        public int ProductId { get; set; }
+
+        public string  ProductName { get; set; }
+
+        public string VendorName { get; set; }
+
+        public int TotalQuantitySold { get; set; }
+
+        public decimal TotalIncome { get; set; }
+    }
+}
