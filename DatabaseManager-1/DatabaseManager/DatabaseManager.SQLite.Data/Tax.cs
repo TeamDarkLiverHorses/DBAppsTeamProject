4ef62cb312108d6f21ddf4af0766976f0eea0cf6
straho99 <strahil.ruychev@gmail.com>
2015-07-28 12:02:12 +0300

    Just a small test with SQLite

---
+++ DatabaseManager-1/DatabaseManager/DatabaseManager.SQLite.Data/Tax.cs
@@ -0,0 +1,16 @@
+﻿namespace DatabaseManager.SQLite.Data
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Tax
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Required]
+        public float TaxSize { get; set; }
+    }
+}
---
+++ DatabaseManager-1/DatabaseManager/DatabaseManager.SQLite.Test/SQLiteTest.cs
@@ -0,0 +1,75 @@
+﻿namespace DatabaseManager.SQLite.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DatabaseManager.Data;
+    using DatabaseManager.Models;
+    using DatabaseManager.SQLite.Data;
+
+    public class SQLiteTest
+    {
+        private static Random rnd = new Random();
+        public static void Main()
+        {
+            Console.WriteLine("Inserted {0} taxes.", UpdateProductsAndTaxes());
+
+            //Console.WriteLine("Deleted {0} taxes.", DeleteAllTaxes());
+
+            var taxesContext = new TaxesContext();
+            var taxes = taxesContext.Taxes;
+            foreach (var tax in taxes)
+            {
+                Console.WriteLine("{0} - {1}", tax.ProductName, tax.TaxSize);
+            }
+        }
+
+        public static int UpdateProductsAndTaxes()
+        {
+            var taxesContext = new TaxesContext();
+            var supermarketsContext = new SupermarketsContext();
+
+            var allProductNames = supermarketsContext.Products.Select(p => p.Name).ToList();
+            var existingProductNames = taxesContext.Taxes.Select(t => t.ProductName).ToList();
+
+            var missingProducts = allProductNames.Except(existingProductNames);
+
+            var productsToInsert = supermarketsContext.Products
+                .Where(p => missingProducts.Contains(p.Name))
+                .Select(p => new { ProductName = p.Name })
+                .ToList();
+
+            foreach (var product in productsToInsert)
+            {
+                taxesContext.Taxes.Add(new Tax()
+                {
+                    ProductName = product.ProductName,
+                    TaxSize = (float)GetRandomNumber(0d, 0.3d)
+                });
+            }
+            taxesContext.SaveChanges();
+
+            return productsToInsert.Count;
+        }
+
+        public static int DeleteAllTaxes()
+        {
+            int count = 0;
+            var context = new TaxesContext();
+            var taxes = context.Taxes;
+            foreach (var tax in taxes)
+            {
+                context.Taxes.Remove(tax);
+                count++;
+            }
+            context.SaveChanges();
+            return count;
+        }
+
+        private static double GetRandomNumber(double minimum, double maximum)
+        {
+            double result =  rnd.NextDouble() * (maximum - minimum) + minimum;
+            return result;
+        }
+    }
+}
